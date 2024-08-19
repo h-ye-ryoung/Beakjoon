6cc40422c4b60aa113a382f2cdd495bbff7ef958
김혜령 <hyeryoung27@soongsil.ac.kr>
2024-08-19 09:00:47 +0900

    class1: b2884

---
+++ src/main/java/class1/b2884/Main.java
@@ -0,0 +1,32 @@
+package main.java.class1.b2884;
+
+// 백준 2884 알람 시계
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+        sc.close();
+
+        if (minute >= 45) {
+            minute = minute - 45;
+            System.out.print(hour + " " + minute);
+        } else {
+            if (hour == 0) {
+                hour = 23;
+            } else {
+                hour = hour - 1;
+            }
+            int temp = minute - 45;
+            minute = 60 + temp;
+            System.out.println(hour + " " + minute);
+        }
+    }
+}
